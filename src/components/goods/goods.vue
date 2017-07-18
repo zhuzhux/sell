0274688e50647a91c6014c48607761882a54c53a
786507017@qq.com <786507017@qq.com>
2017-07-18 17:57:33 +0800

    Initial commit

---
+++ src/components/goods/goods.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>I am goods</div>
+</template>
+
+<script type="text/ecmascript6">
+  export default{
+
+  };
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus" type="text/stylus">
+
+</style>
