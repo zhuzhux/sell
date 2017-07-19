0449dceaa89966dfbb3f30684ddf32825a1737b1
786507017@qq.com <786507017@qq.com>
2017-07-20 00:28:14 +0800

    cartcontrol
    cartcontrol

--- src/components/goods/goods.vue
+++ src/components/goods/goods.vue
@@ -27,8 +27,10 @@
                   <span>好评率{{food.rating}}%</span>
                 </div>
                 <div class="price">
-                  <span class="now">￥{{food.price}}</span><span class="old"
-                                                                v-show="food.oldPrice">￥{{food.oldPrice}}</span>
+                  <span class="now">￥{{food.price}}</span><span class="old" v-show="food.oldPrice">￥{{food.oldPrice}}</span>
+                </div>
+                <div class="cartcontrol-wrapper">
+                  <cartcontrol :food="food"></cartcontrol>
                 </div>
               </div>
             </li>
@@ -44,6 +46,7 @@
 <script type="text/ecmascript-6">
   import BScroll from 'better-scroll';
   import shopcart from 'components/shopcart/shopcart';
+  import cartcontrol from 'components/cartcontrol/cartcontrol';
 
   const ERR_OK = 0;
 
@@ -119,7 +122,8 @@
       }
     },
     components: {
-      shopcart
+      shopcart,
+      cartcontrol
     }
 
   };
